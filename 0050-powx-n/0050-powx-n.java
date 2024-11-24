1427f0e22922d7e16199972d365883a1f3fc11d9
Arijit2003 <91794464+Arijit2003@users.noreply.github.com>
2024-11-24 13:33:41 +0530

    Time: 4 ms (5.47%), Space: 52.9 MB (6.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,19 @@
-import java.lang.Math;
-
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if(x==1 || (x==-1 &&  n>0))return x;
+        if(x==-1 && n<0)return 1;
+        if(n==Integer.MAX_VALUE||n==Integer.MIN_VALUE)return 0;
+        double[] dp = new double[Math.abs(n)+1];
+        Arrays.fill(dp,-1);
+        if(n<0){
+            return 1/pow(x,Math.abs(n),dp);
+        }return pow(x,n,dp);
+    }
+    public double pow(double x, int n,double[]dp){
+        if(n==0)return dp[n]=1;
+        if(n==1)return dp[n]=x;
+        if(dp[n]!=-1)return dp[n];
+        if(n%2==0)return dp[n]=pow(x,n/2,dp)*pow(x,n/2,dp);
+        else return dp[n]=x*pow(x,n-1,dp);
     }
 }
