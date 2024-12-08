de031012cd6a41a5711df0a1be1b81d35f2c5f30
Arijit2003 <91794464+Arijit2003@users.noreply.github.com>
2024-12-08 11:42:58 +0530

    Time: 3 ms (78.87%), Space: 43.4 MB (31.66%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -3,29 +3,10 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
         Arrays.sort(nums);
         res = new ArrayList<>();
-        // combi(candidates,candidates.length,target, new ArrayList<Integer>());
-        // List<List<Integer>> temp = new ArrayList<>();
-        // temp.addAll(res);
-        // return temp; 
         combination(nums,0,nums.length,target,new ArrayList<Integer>());
-        return res;
-        
+        return res;   
     }
-    public void combi(int[] cand, int n, int target, ArrayList<Integer> temp){
-        if(target==0){
-            res.add(new ArrayList<>(temp));
-            return ;
-        }
-        if(n==0 && target!=0) return ;
-        //not take
-        combi(cand, n-1, target, temp);
-        //take
-        if(target>=cand[n-1]){
-            temp.add(cand[n-1]);
-            combi(cand,n-1,target-cand[n-1],temp);
-            temp.remove(temp.size()-1);
-        }
-    }
+    
     public void combination(int[] nums, int idx,int n,int target, ArrayList<Integer> temp ){
         if(target==0){
             res.add(new ArrayList<>(temp));
