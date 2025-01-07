7e745966165d6aae93ed322f886c2c29cb2c6019
Arijit2003 <91794464+Arijit2003@users.noreply.github.com>
2025-01-07 10:40:14 +0530

    Time: 4 ms (41.66%), Space: 43.9 MB (49.02%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,6 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer,Integer> hmap = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty() && nums2[i]>st.peek())st.pop();
+            if(st.isEmpty()){
+                hmap.put(nums2[i],-1);
+                st.push(nums2[i]);
+            }else {
+                hmap.put(nums2[i],st.peek());
+                st.push(nums2[i]);
+            } 
+        }
+        for(int i=0;i<nums1.length;i++){
+            nums1[i]=hmap.get(nums1[i]);
+        }
+        return nums1;
+    }
+}
+/*
+HashMap<Integer,Integer> hmap = new HashMap<>();
         boolean flag=false;
         for(int i=0;i<nums2.length;i++){
             flag=false;
@@ -18,5 +37,4 @@ class Solution {
             res[i]=hmap.get(nums1[i]);
         }
         return res;
-    }
-}
+*/
