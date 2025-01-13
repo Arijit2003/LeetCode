648102fb7790a203cc7ce8188160c8cde20838ec
Arijit2003 <91794464+Arijit2003@users.noreply.github.com>
2025-01-13 11:16:01 +0530

    Time: 123 ms (7.08%), Space: 56.8 MB (90%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        int left[]=new int[n], right[]=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<heights.length;i++){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i])st.pop();
+            if(st.isEmpty())left[i]=-1;
+            else left[i]=st.peek();
+            st.push(i);
+        }
+        nextSmallerElement(heights,right);
+        int maxarea=0,area=0;
+        for(int i=0;i<n;i++){
+            area=heights[i]*(right[i]-left[i]-1);
+            maxarea=Math.max(area,maxarea);
+        }
+        return maxarea;
+        
+    }
+    public void previousSmallerElement(int[]arr, int left[]){
+        
+    }
+
+    public void nextSmallerElement(int[]arr, int right[]){
+        Stack<Integer> st=new Stack<>();
+        int n=arr.length;
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i])st.pop();
+            if(st.isEmpty())right[i]=n;
+            else right[i]=st.peek();
+            st.push(i);
+        }
+    }
+}
